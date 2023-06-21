ef91e0fac9975e0307cc8e09eb733527d7f768ca
Elizabeth Lowry <eglowry@gmail.com>
2023-06-21 08:58:06 -0600

    more styling

--- react-app/src/components/DeleteProductModal/index.js
+++ react-app/src/components/DeleteProductModal/index.js
@@ -2,9 +2,10 @@ import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { useModal } from "../../context/Modal";
 import { deleteProductThunk, getProductsForShopOwnerThunk } from "../../store/product";
+import './DeleteProductModal.css'
 
 
-function DeleteProductModal({id}) {
+function DeleteProductModal({ id }) {
     const { closeModal } = useModal();
     const dispatch = useDispatch();
     const history = useHistory();
@@ -30,10 +31,16 @@ function DeleteProductModal({id}) {
             <div>
                 <h2>Confirm Delete</h2>
                 <h3>Are you sure you want to delete your product?</h3>
-                <button
-                    onClick={deleteProduct}>Yes</button>
-                <button
-                    onClick={closeDelete}>No</button>
+                <div>
+                    <button
+                        onClick={deleteProduct}
+                        className="deletebutton"
+                        >Yes</button>
+                    <button
+                        onClick={closeDelete}
+                        className="deletebutton"
+                        >No</button>
+                </div>
             </div>
         </div>
     )
