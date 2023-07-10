8faf9aac962203a77cf9f3fd2ab3beb1d32849ff
Elizabeth Lowry <eglowry@gmail.com>
2023-07-10 16:16:41 -0600

    added links to specific products on landing page

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -4,7 +4,11 @@ import { useHistory } from 'react-router-dom'
 import { getAllProductsThunk } from '../../store/product'
 import './LandingPage.css'
 import { getAllShopsThunk } from '../../store/shop'
+import { NavLink } from 'react-router-dom/cjs/react-router-dom.min'
 
+export function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
 
 const LandingPage = () => {
     const dispatch = useDispatch()
@@ -14,32 +18,75 @@ const LandingPage = () => {
     console.log('products', products)
 
 
+
+
     useEffect(() => {
         dispatch(getAllProductsThunk())
         dispatch(getAllShopsThunk())
 
     }, [dispatch])
 
+
+
     if (!products) return <h1>Products loading...</h1>
     let productArray = Object.values(products)
-    return (
-        <div className='productCardContainer' >
-            {productArray?.map(product => (
-                <div key={product.id} className='productCard' onClick={(e) => {
-                    history.push(`/products/${product.id}`)
-                }
-                }>
-                    <div className='imageContainerCard'>
-                        <img src={product.main_image} alt='' className='cardImage' />
-                    </div>
-                    <div className='cardDetails'>
-                        <div className='productPrice'>${product.price.toFixed(2)}</div>
-                        {/* <p>{product.name}</p> */}
 
+    //randomly pick home product
+    const homeProducts = productArray.filter(product => product.category === "Home & Living")
+    const featuredHomeProduct = homeProducts[getRandomInt(homeProducts.length)]
+
+    //randomly pick accessory product
+    const accessoryProducts = productArray.filter(product => product.category === "Jewelry & Accessories")
+    const featuredAccessoryProduct = accessoryProducts[getRandomInt(accessoryProducts.length)]
+
+    //randomly pick clothing product
+    const clothingProducts = productArray.filter(product => product.category === "Clothing & Shoes")
+    const featuredClothingProduct = clothingProducts[getRandomInt(clothingProducts.length)]
+
+    //randomly pick art product
+    const artProducts = productArray.filter(product => product.category === "Art & Collectibles")
+    const featuredArtProduct = artProducts[getRandomInt(artProducts.length)]
+
+    return (
+        <div>
+            <div className='landingbanner'>
+                <h2 className='bannertext'>Discover fresh finds from creative sellers!</h2>
+            </div>
+            <div className='categorydisplay'>
+                <NavLink className='landingnav' to={`/products/${featuredHomeProduct?.id}`}><div className='circleprod'>
+                    <img src={featuredHomeProduct?.main_image} alt='' className='landingimage' />
+                    Home & Living
+                </div></NavLink>
+                <NavLink className='landingnav' to={`/products/${featuredAccessoryProduct?.id}`}><div className='circleprod'>
+                    <img src={featuredAccessoryProduct?.main_image} alt='' className='landingimage' />
+                    Jewelry & Accessories</div></NavLink>
+                <NavLink className='landingnav' to={`/products/${featuredClothingProduct?.id}`}><div className='circleprod'>
+                    <img src={featuredClothingProduct?.main_image} alt='' className='landingimage' />
+                    Clothing & Shoes</div></NavLink>
+                <NavLink className='landingnav' to={`/products/${featuredArtProduct?.id}`}><div className='circleprod'>
+                    <img src={featuredArtProduct?.main_image} alt='' className='landingimage' />
+                    Art & Collectibles</div></NavLink>
+
+
+            </div>
+            <div className='productCardContainer' >
+                {productArray?.map(product => (
+                    <div key={product.id} className='productCard' onClick={(e) => {
+                        history.push(`/products/${product.id}`)
+                    }
+                    }>
+                        <div className='imageContainerCard'>
+                            <img src={product.main_image} alt='' className='cardImage' />
+                        </div>
+                        <div className='cardDetails'>
+                            <div className='productPrice'>${product.price.toFixed(2)}</div>
+                            {/* <p>{product.name}</p> */}
+
+                        </div>
                     </div>
-                </div>
-            ))
-            }
+                ))
+                }
+            </div>
         </div>
     )
 
